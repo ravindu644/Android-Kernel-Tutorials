6b84855d03bcbeedbacd93d6a6fe4bb6a1f3b7ee
Ravindu Deshan <droidcasts@protonmail.com>
2025-03-15 19:02:25 +0530

    build scripts: added script for GKI kernels
    Signed-off-by: Ravindu Deshan <droidcasts@protonmail.com>

---
+++ build_scripts/build_GKI.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo -e "\n[INFO]: BUILD STARTED..!\n"
+
+export RDIR="$(pwd)"
+export ARCH=arm64
+export KBUILD_BUILD_USER="@ravindu644"
+
+# Install the requirements for building the kernel when running the script for the first time
+if [ ! -f ".requirements" ]; then
+    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
+        default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
+        python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
+        make repo cpio kmod openssl libelf-dev pahole libssl-dev libarchive-tools zstd --fix-missing && wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb && sudo dpkg -i libtinfo5_6.3-2ubuntu0.1_amd64.deb && touch .requirements
+fi
+
+# Create necessary directories
+mkdir -p "${RDIR}/out" "${RDIR}/build" "${HOME}/toolchains"
+
+#init neutron-clang
+if [ ! -d "${HOME}/toolchains/neutron-clang" ]; then
+    echo -e "\n[INFO] Cloning Neutron-Clang Toolchain\n"
+    mkdir -p "${HOME}/toolchains/neutron-clang" && cd "${HOME}/toolchains/neutron-clang"
+    curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman" && chmod +x antman
+    bash antman -S && bash antman --patch=glibc
+    cd "${RDIR}"
+fi
+
+#init arm gnu toolchain
+if [ ! -d "${HOME}/toolchains/gcc" ]; then
+    echo -e "\n[INFO] Cloning ARM GNU Toolchain\n"
+    mkdir -p "${HOME}/toolchains/gcc" && cd "${HOME}/toolchains/gcc"
+    curl -LO "https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz"
+    tar -xf arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz
+    cd "${RDIR}"
+fi
+
+# Export toolchain paths
+export PATH="${PATH}:${HOME}/toolchains/neutron-clang/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HOME}/toolchains/neutron-clang/lib"
+
+# Set cross-compile environment variables
+export BUILD_CROSS_COMPILE="${HOME}/toolchains/gcc/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-"
+export BUILD_CC="${HOME}/toolchains/neutron-clang/bin/clang"
+
+# Build options for the kernel
+export BUILD_OPTIONS="
+-C ${RDIR} \
+O=${RDIR}/out \
+-j$(nproc) \
+ARCH=arm64 \
+CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
+CC=${BUILD_CC} \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+LLVM=1 \
+LLVM_IAS=1 \
+AR=${HOME}/toolchains/neutron-clang/bin/llvm-ar \
+NM=${HOME}/toolchains/neutron-clang/bin/llvm-nm \
+LD=${HOME}/toolchains/neutron-clang/bin/ld.lld \
+STRIP=${HOME}/toolchains/neutron-clang/bin/llvm-strip \
+OBJCOPY=${HOME}/toolchains/neutron-clang/bin/llvm-objcopy \
+OBJDUMP=${HOME}/toolchains/neutron-clang/bin/llvm-objdump \
+READELF=${HOME}/toolchains/neutron-clang/bin/llvm-readelf \
+HOSTCC=${HOME}/toolchains/neutron-clang/bin/clang \
+HOSTCXX=${HOME}/toolchains/neutron-clang/bin/clang++ \
+"
+
+build_kernel(){
+    # Make default configuration.
+    make ${BUILD_OPTIONS} gki_defconfig
+
+    # Configure the kernel (GUI)
+    make ${BUILD_OPTIONS} menuconfig
+
+    # Build the kernel
+    make ${BUILD_OPTIONS} Image || exit 1
+
+    # Copy the built kernel to the build directory
+    cp "${RDIR}/out/arch/arm64/boot/Image" "${RDIR}/build"
+
+    echo -e "\n[INFO]: BUILD FINISHED..!"
+}
+build_kernel
--- toolchains/README.md
+++ toolchains/README.md
@@ -30,7 +30,7 @@
 - Tested on Linux 5.10.198
 - Usage: [here](https://github.com/ravindu644/android_kernel_s23fe/blob/6413302587aa865a16bc04a9a835479ce3a4beee/build_kernel.sh#L15)
 
-#### 5. **Linux 5.15:** [neutron-clang](https://github.com/Neutron-Toolchains/antman), [arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu](https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz)
+#### 5. **Linux 5.15 & any other GKI Kernel:** [neutron-clang](https://github.com/Neutron-Toolchains/antman), [arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu](https://developer.arm.com/-/media/Files/downloads/gnu/14.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu.tar.xz)
 
 - Tested on Linux 5.15.123 - 5.15.149
 - Usage: [here](https://github.com/ravindu644/android_kernel_m145f_common/blob/c3a3a4ab9df28005200fa516f1a8ed9913bf50d6/build.sh#L27)
