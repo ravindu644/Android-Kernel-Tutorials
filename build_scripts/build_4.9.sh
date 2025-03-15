c5ef37bcb152201a8e1d6a02c4cfda7ccd03b3bc
Ravindu Deshan <droidcasts@protonmail.com>
2025-03-15 13:31:47 +0530

    build_4.9: enhanced docs
    Signed-off-by: Ravindu Deshan <droidcasts@protonmail.com>

--- build_scripts/build_4.9.sh
+++ build_scripts/build_4.9.sh
@@ -6,7 +6,7 @@ export RDIR="$(pwd)"
 export ARCH=arm64
 export KBUILD_BUILD_USER="@ravindu644"
 
-# Install the requirements for building the kernel
+# Install the requirements for building the kernel when running the script for the first time
 if [ ! -f ".requirements" ]; then
     sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
         default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
