3729985ac5cc09e2bbde8057ce5e5297d3a71bff
Ravindu Deshan <droidcasts@protonmail.com>
2025-04-05 20:54:20 +0530

    docker: kernel-builder: function to re-install the container
    Signed-off-by: Ravindu Deshan <droidcasts@protonmail.com>

--- docker/kernel-builder.sh
+++ docker/kernel-builder.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
+RED="\e[31m"
+RESET="\e[0m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
-    echo -e "\e[31mInstall docker first..!\e[0m"
+    echo -e "${RED}Error: Docker is not installed! Please install Docker first.${RESET}"
     exit 1
 fi
 
-# Check if .installed file exists
-if [ -f ".installed" ]; then
-    echo -e "\e[32mkernel-builder is already installed\e[0m"
+# Check if --reinstall flag is provided
+if [[ "$1" == "--reinstall" ]]; then
+    echo -e "${YELLOW}Removing previous kernel-builder container and volume...${RESET}"
+    
+    # Remove any containers using the kernel-builder image with its volumes
+    docker ps -a -q --filter ancestor=kernel-builder | xargs -r docker rm -f
+    docker volume rm kernel-builder-data 2>/dev/null
+    rm -f .installed
+    
+    # Reinstall
+    echo -e "${GREEN}Reinstalling kernel-builder...${RESET}"
+    xz -d -c kernel-builder.tar.xz | docker load && touch ".installed"
 else
-    echo -e "\e[32mInstalling kernel-builder...\e[0m"
-    ( xz -d -c kernel-builder.tar.xz | docker load ) && touch ".installed"
+    # Normal flow: check if installed, install if needed
+    if [ -f ".installed" ]; then
+        echo -e "${GREEN}kernel-builder is already installed${RESET}"
+    else
+        # First time installation
+        echo -e "${GREEN}Installing kernel-builder...${RESET}"
+        xz -d -c kernel-builder.tar.xz | docker load && touch ".installed"
+    fi
 fi
 
-# Run the Docker container
-docker run -it -v kernel-builder-data:/home/kernel-builder -v "$HOME:/home/kernel-builder/$USER" kernel-builder
+# Run the container
+echo -e "${GREEN}Launching kernel-builder container...${RESET}"
+docker run -it --privileged \
+    -v kernel-builder-data:/home/kernel-builder \
+    -v "$HOME:/home/kernel-builder/$USER" \
+    kernel-builder
